8c873122e6d17bb38873a67b823d199fb7920ddd
Brian Brown <brian@thekompanee.com>
2019-06-30 21:38:35 -0500

    Admin view of widget

---
+++ src/WordPress/Views/Admin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of WordPress Widget Boilerplate
+ * (c) Tom McFarlin <tom@tommcfarlin.com>
+ *
+ * This source file is subject to the GPL license that is bundled
+ * with this source code in the file LICENSE.
+ */
+?>
+<p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>">
+        <?php _e( 'Title', 'bb-nasa-api-random-posts' ); ?>
+    </label>
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+</p>
